3af580ba5657e29f9f6753cfc48f01a9d7b55c29
Casey Hawkins <hawkcase@oregonstate.edu>
2022-06-27 10:21:45 -0500

    Setup for A0.6

---
+++ Prng.py
@@ -0,0 +1,9 @@
+while True: 
+	# Sleep for 1 second.
+# Open file prng-service.txt
+# Read file 
+# If line in file is “run”:
+# Generate random number 
+# Erase “run” from prng-service.txt
+# Write random number in to prng-service.txt
+# Close file 
